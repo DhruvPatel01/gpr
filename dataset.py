d79d00566141cc2e4f0b0c1b3c06352f48e1b1fc
Dhruv Patel <dhruvpatel@iisc.ac.in>
2019-03-12 22:27:48 +0530

    datasetv1 ready

---
+++ dataset.py
@@ -0,0 +1,52 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import pathlib
+import pickle
+import to_window
+
+class DS_v1(Dataset):
+    def __init__(self, pickle_dir, embed_dir,
+                 max_window=10):
+        pickle_path = pathlib.Path(pickle_dir)
+        embd_dir = pathlib.Path(embed_dir)
+        pickles = pickle_path.glob('*.pkl')
+        Xs = []
+        Ys = []
+        Zs = []
+        lengths = []
+        tokens = []
+        titles = []
+        for pkl in pickles:
+            with open(pkl, 'rb') as f:
+                data = pickle.load(f)
+            embed = np.load(embd_dir / (pkl.stem + '.npy'))
+            pronoun_ind = data['pronoun_index']
+            pronoun_vect = embed[pronoun_ind]
+            ans_ind = data['ans_index']
+            ans = data['ans']
+            ys = np.zeros(len(embed), dtype=np.int64)
+            ys[ans_ind:ans_ind+len(ans)] = 1
+            assert ys[pronoun_ind] == 0
+            newx, newy, lens = to_window.to_window_cbow(embed, ys, pronoun_ind, len(ans)+3)
+            Xs.append(newx)
+            Ys.append(newy)
+            Zs.append(pronoun_vect)
+            tokens.append(data['tokens'])
+            titles.append(pkl.stem)
+            lengths.append(lens)
+        self.Xs = Xs
+        self.Ys = Ys
+        self.Zs = Zs
+        self.tokens = tokens
+        self.titles = titles
+
+    def __len__(self):
+        return len(self.Zs)
+
+    def __getitem__(self, i):
+        return self.Xs[i], self.Ys[i], self.Zs[i], self.tokens[i], self.titles[i]
+
+if __name__ == '__main__':
+    ds = DS_v1('data/gap/pickles/dev', 'data/gap/embeds')
+
--- to_window.py
+++ to_window.py
@@ -1,11 +1,19 @@
 import numpy as np
 
-def to_window_cbow(matrix, max_window=10):
-    M, N  = matrix.shape
-    vectors = []
+def to_window_cbow(x, y, ignore, max_window=10):
+    M, N  = x.shape
+    new_x = []
+    new_y = []
+    additional = []
     for i in range(1, max_window+1):
         for j in range(M - i+1):
-            vec = matrix[j:j+i, :].sum(0)
-            vectors.append(vec)
-
-    return np.array(vectors)
+            if j <= ignore < j+i:
+                continue
+            vec = x[j:j+i, :].sum(0)
+            new_x.append(vec)
+            additional.append((j, i))
+            if np.all(y[j:j+i]):
+                new_y.append(1)
+            else:
+                new_y.append(0)
+    return np.array(new_x), np.array(new_y), additional
