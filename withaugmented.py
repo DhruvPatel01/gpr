75347e9fd0cce41ea900a96bbaf6d00de07710b2
Dhruv Patel <dhruvpatel@iisc.ac.in>
2019-04-29 21:11:58 +0530

    new code to support testing

--- withaugmented.py
+++ withaugmented.py
@@ -85,7 +85,7 @@ class Model(nn.Module):
             nn.Linear(BERT_DIM*3, 64),
             nn.ReLU(),
             nn.Dropout(dropout),
-            # nn.BatchNorm1d(64),
+            nn.BatchNorm1d(64),
             nn.Linear(64, 32),
             nn.ReLU(),
             nn.Dropout(dropout),
@@ -107,16 +107,19 @@ class Model(nn.Module):
         hb = self.linear(b)
         return torch.cat([ha, hb, torch.zeros_like(ha)], -1)
 
-
-def train(tdl, vdl, layer, patience=5, logdir='.'):
+def train(tdl, vdl, args):
     dev = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     bert = BertModel.from_pretrained('bert-large-cased').to(dev)
     model = Model(dropout=.5).to(dev)
+    if args.pre_model:
+        model.load_state_dict(torch.load(args.logdir + '/' + args.pre_model))
     loss_fn = nn.CrossEntropyLoss()#torch.tensor([0.15, 0.15, .7]).to(dev))
     # optim = torch.optim.SGD(model.parameters(), lr=0.001, weight_decay=.001)
     optim = torch.optim.Adam(model.parameters(), weight_decay=0.001)
-    writer = SummaryWriter(logdir+'/Log')
+    if args.pre_opt:
+        optim.load_state_dict(torch.load(args.logdir + '/' + args.pre_opt))
+    writer = SummaryWriter(args.logdir+'/Log')
 
     best_val_loss = 100
     last_updated = 0
@@ -127,7 +130,7 @@ def train(tdl, vdl, layer, patience=5, logdir='.'):
             sent, spans, y = [e.to(dev) for e in elem]
             with torch.no_grad():
                 embd, _ = bert(sent)
-            embd = embd[layer]
+            embd = embd[args.bert_layer]
             a_mask = torch.arange(embd.size(1)).to(dev)
             b_mask = torch.arange(embd.size(1)).to(dev)
             a_mask = a_mask.expand(embd.size(0), -1)
@@ -171,14 +174,57 @@ def train(tdl, vdl, layer, patience=5, logdir='.'):
             if best_val_loss > loss:
                 last_updated = 0
                 best_val_loss = loss
-                torch.save(model.state_dict(), logdir+'/best_model-%.2f.pt'%best_val_loss)
-                torch.save(optim.state_dict(), logdir+'/best_optim-%.2f.pt'%best_val_loss)
+                torch.save(model.state_dict(), args.logdir+'/best_model-%.2f.pt'%best_val_loss)
+                torch.save(optim.state_dict(), args.logdir+'/best_optim-%.2f.pt'%best_val_loss)
                 best = '*'
             else:
                 last_updated += 1
-                if last_updated > patience:
+                if last_updated > args.patience:
                     return model
             tqdm.tqdm.write("Epoch: %d, Tr.loss: %.4f  Vl.loss: %.4f %s" % (epoch, total_loss, loss, best))
+
+def test(ds, args, labels=True, out='test_log.txt'):
+    dev = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    bert = BertModel.from_pretrained('bert-large-cased').to(dev)
+    model = Model(dropout=.5).to(dev)
+    model.load_state_dict(torch.load(args.logdir + '/' + args.pre_model))
+
+    log_loss = 0
+    log_file = open(args.logdir + '/' + out, 'w')
+    print("ID,A,B,NEITHER", file=log_file)
+    model.eval()
+    with torch.no_grad():
+        for elem in tqdm.tqdm(ds, "Testing"):
+            if labels:
+                sent, spans, y, name = elem
+            else:
+                sent, spans, name = elem
+            sent = sent.to(dev).unsqueeze(0)
+            spans = spans.to(dev).unsqueeze(0)
+            embd, _ = bert(sent)
+            embd = embd[args.bert_layer]
+            a_mask = torch.arange(embd.size(1)).to(dev)
+            b_mask = torch.arange(embd.size(1)).to(dev)
+            a_mask = a_mask.expand(embd.size(0), -1)
+            b_mask = b_mask.expand(embd.size(0), -1)
+            a_mask = (a_mask < spans[:, 1].view(-1, 1)) & ~(a_mask < spans[:, 0].view(-1, 1))
+            b_mask = (b_mask < spans[:, 3].view(-1, 1)) & ~(b_mask < spans[:, 2].view(-1, 1))
+            a_mask, b_mask = a_mask.to(torch.float32).unsqueeze(-1), b_mask.to(torch.float32).unsqueeze(-1)
+
+            a, b = (embd * a_mask).sum(1), (embd * b_mask).sum(1)
+            a /= (spans[:, 1] - spans[:, 0]).to(torch.float32).view(-1, 1)
+            b /= (spans[:, 3] - spans[:, 2]).to(torch.float32).view(-1, 1)
+            p = embd[range(embd.size(0)), spans[:, 4]]
+            logits = model(a, b, p)
+            prob = tuple(torch.softmax(logits, -1).cpu()[0])
+            print('%s,%.4f,%.4f,%.4f' % (name, *prob), file=log_file)
+            if labels:
+                logprobs = torch.log_softmax(logits, -1)
+                log_loss -= float(logprobs[0, y])
+    print("Loss: %.4f"%(log_loss/len(ds)))
+    log_file.close()
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--bert_layer', '-l', '--bl', default=17, type=int)
@@ -187,22 +233,34 @@ def parse_args():
     parser.add_argument('-w', action='store_true', help='include winobias')
     parser.add_argument('-d', action='store_true', help='include defpr')
     parser.add_argument('--logdir', default='.')
-
+    parser.add_argument('--pre_model')
+    parser.add_argument('--pre_opt')
+    parser.add_argument('--test', action='store_true')
+    parser.add_argument('--test2', action='store_true')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-
-    vds = lee.DS('./data/gap-coreference/pickles/bert-large-val-%d.pkl'%args.bert_layer,
-                 './data/gap-coreference/gap-validation.tsv', 0)
-    vdl = DataLoader(vds, 5)
-
     tokenizer = BertTokenizer.from_pretrained('bert-large-cased', do_lower_case=False)
-    ds = dataset.DS_Augmented('./data/gap-coreference/gap-test.tsv', tokenizer)
-    tds = ds
-    if args.d:
-        defds = dataset.DS_Augmented('./data/defpr/defpr.csv', tokenizer, 0)
-        tds = ConcatDataset([tds, defds])
-    tdl = DataLoader(tds, args.batch_size, True, collate_fn=ds.collate_fn)
 
-    train(tdl, vdl, patience=args.patience, logdir=args.logdir, layer=args.bert_layer)
+    if args.test:
+        ds = dataset.DS_Augmented('./data/gap-coreference/gap-development-corrected.tsv',
+                                  tokenizer, 0, return_ids=True)
+        test(ds, args, True, out='test_stage_1.csv')
+    elif args.test2:
+        ds = dataset.DS_Augmented('./data/gap-coreference/test_stage_2.tsv',
+                                  tokenizer, 0, False, return_ids=True)
+        test(ds, args, False, out='test_stage_2.csv')
+    else:
+        vds = lee.DS('./data/gap-coreference/pickles/bert-large-val-%d.pkl'%args.bert_layer,
+                    './data/gap-coreference/gap-validation-corrected.tsv', 0)
+        vdl = DataLoader(vds, 5)
+
+        ds = dataset.DS_Augmented('./data/gap-coreference/gap-test-corrected.tsv', tokenizer)
+        tds = ds
+        if args.d:
+            defds = dataset.DS_Augmented('./data/defpr/defpr.csv', tokenizer, 0)
+            tds = ConcatDataset([tds, defds])
+        tdl = DataLoader(tds, args.batch_size, True, collate_fn=ds.collate_fn)
+
+        train(tdl, vdl, args)
